625ed224e358af77d2dbaee2c54dd4a6be36b523
Alistairbear <Alistair_westmacott@outlook.com>
2019-05-20 21:44:34 +0100

    minor changes to renderer
    Very little changes made here, nothing significant.

--- renderer.cpp
+++ renderer.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
 using namespace std;
 
+#include "sphere.h"
+#include "photon.h"
+
 int createImage() {
-    FILE * fp = fopen ("file.bmp", "wb");
-    fwrite(bmpfile_header, sizeof(bmpfile_header), 1, fp);
-    fwrite(bmp_dib_v3_header, sizeof(bmp_dib_v3_header_t), 1, fp);
+    
+}
 
-    for (int i = 0; i < 200; i++)  {
-        for (int j = 0; j < 200; j++) {
-            fwrite(&image[j][i][2], 1, 1, fp);
-            fwrite(&image[j][i][1], 1, 1, fp);
-            fwrite(&image[j][i][0], 1, 1, fp);
-        }
-    }
-
-    fclose(fp);
+int main() {
+    sphere lightSource;
+    
 }
